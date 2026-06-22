ad2ade62b57217d2e9a115df2795d51230b51643
tarunmaanam-glitch <tarunmaanam@gmail.com>
2026-06-22 09:48:05 +0530

    Refactor academic_agent to use ACADEMIC_KB

--- university_chatbot/agents/academic_agent.py
+++ university_chatbot/agents/academic_agent.py
@@ -1,9 +1,9 @@
 from langchain_core.messages import AIMessage
 from utilities.llm import llm
-from knoledge_base.placement_kb import PLACEMENT_KB
+from knoledge_base.academic_kb import ACADEMIC_KB
 def academic_agent(state):
     question=state["messages"][-1].content
-    context = "\n".join(PLACEMENT_KB.values())
+    context = "\n".join(ACADEMIC_KB.values())
 
     result = llm.invoke(
         f"""
